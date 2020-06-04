a0d72292bdc6f4ff91709a12e49000e45630a686
Ariel Huynh <arielhuynh@ymail.com>
2020-06-03 19:40:46 -0700

    removed files

--- client/app.js
+++ client/app.js
@@ -11,13 +11,6 @@ app.use(bodyParser.json());
 
 app.use(cors());
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); 
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
-
 
 
 app.listen(PORT,()=>{
